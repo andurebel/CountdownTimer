33770122a248ae5cf29521da21ecda483e340431
Andu <66033930+andurebel@users.noreply.github.com>
2021-02-02 22:40:40 +0200

    Update script.js
    Changed the year

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 jan 2021";
+const newYears = "1 jan 2022";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
